a327ea00aa677d4dcf19856396d6666a71579941
Ya, Wang <shuyu.ya.wang@gmail.com>
2017-10-14 00:10:34 -0700

    add script to make windows release

---
+++ make_releases_win.bash
@@ -0,0 +1,9 @@
+rm -rf build/ dist/ *.spec
+pyinstaller --paths modules/ --add-data="data;data" tcm_lib_search.py
+pyinstaller --paths modules/ \
+            --add-data="data;data" \
+            --add-data="modules/jieba/dict.txt;jieba/" \
+            --add-data="modules/jieba/analyse/idf.txt;jieba/analyse/" \
+            --hidden-import sklearn.neighbors.typedefs \
+            extract_formula.py
+# rm -rf build/ tcm_lib_search.spec
